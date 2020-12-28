898761b90a5fa4fa0efca30cf41d21a3ffbec740
katty oliveira <kattyolv@gmail.com>
2020-12-28 15:31:30 +0000

    created client sign in controller and implemented post method

---
+++ src/com/kattyolv/don/mart/api/controller/ClientSignInController.java
@@ -0,0 +1,62 @@
+package com.kattyolv.don.mart.api.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.kattyolv.don.mart.api.dao.DAOClient;
+import com.kattyolv.don.mart.api.model.Client;
+
+
+@WebServlet("/client/sign-in")
+public class ClientSignInController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		try {
+			DAOClient clientDAO = new DAOClient();
+			
+			String email = request.getParameter("email");
+			String password = request.getParameter("password");
+
+			if(!email.replaceAll("\\s+", "").equals("") &&
+					!password.replaceAll("\\s+", "").equals("")) {
+				
+				Client client = clientDAO.selectClientByEmailAndPassword(email, password);
+				
+				if(client != null) {
+					
+					Gson gson = new Gson();
+					String clientJson = gson.toJson(client);
+					
+					PrintWriter out = response.getWriter();
+					out.println(clientJson);
+					
+					response.setStatus(200);
+					response.setContentType("application/json");
+				}
+				else {
+					response.setStatus(401);
+				}
+				
+				return;
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			response.setStatus(500);
+		}
+		
+		response.setStatus(400);
+		
+	}
+
+}
